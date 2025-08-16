c44f02a237cadb2f54301257de998c0a4a41d7cc
Subhash Kumar <subhashkushwah134@gmail.com>
2025-08-16 15:40:47 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,27 +15,20 @@ const Footer = () => {
           {/* Social Media Links */}
           <div className="text-center">
             <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
-            <div className="flex justify-center md:justify-start space-x-4">
-              <a href="#" className="hover:text-blue-500 transition duration-300">
-                <FaFacebook size={24} />
-              </a>
-              <a href="#" className="hover:text-blue-400 transition duration-300">
-                <FaTwitter size={24} />
-              </a>
-              <a href="#" className="hover:text-pink-500 transition duration-300">
+            <div className="flex justify-center md:justify-start space-x-4">              
+              <a href="https://instagram.com/subhash_kushwaah" className="hover:text-pink-500 transition duration-300">
                 <FaInstagram size={24} />
               </a>
-              <a href="#" className="hover:text-blue-700 transition duration-300">
+              <a href="https://www.linkedin.com/in/subhash-kumar-f98z" className="hover:text-blue-700 transition duration-300">
                 <FaLinkedin size={24} />
               </a>
             </div>
-          </div>
-
+          </div> 
           {/* Contact Info */}
           <div className="text-center md:text-left">
             <h3 className="text-lg font-semibold mb-4">Contact</h3>
-            <p className="text-gray-400">Email: info@examvault.com</p>
-            <p className="text-gray-400">Phone: +91 12345 67890</p>
+            <p className="text-gray-400">Email: subhashkushwah134@gmail.com</p>
+            <p className="text-gray-400">Phone: +91 9855369230</p>
           </div>
         </div>
 
